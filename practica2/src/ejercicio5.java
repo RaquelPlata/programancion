4954254bade13fb5a373c42552d68da50be4ade0
RaquelPlata <raquelplataramos@gmail.com>
2025-10-06 13:48:05 +0200

    ejercicio 5 finalizado

---
+++ practica2/src/ejercicio5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ejercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Pedimos el tamaño del array
+        System.out.print("Introduce el tamaño del array: ");
+        int tamaño = sc.nextInt();
+
+        int[] array = new int[tamaño];
+        int suma = 0;
+
+        System.out.println("Valores del array:");
+        for (int i = 0; i < tamaño; i++) {
+            array[i] = (int) (Math.random() * 10); // aleatorio entre 0 y 9
+            System.out.println("Posición " + i + ": " + array[i]);
+            suma += array[i];
+        }
+
+        System.out.println("\nLa suma de todos los valores es: " + suma);
+
+    }
+}
