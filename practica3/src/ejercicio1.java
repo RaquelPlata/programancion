1966be7aefba27ac39404e401ee8f62a1666e9b4
RaquelPlata <raquelplataramos@gmail.com>
2025-10-27 13:46:20 +0100

    practica3

---
+++ practica3/src/ejercicio1.java
@@ -0,0 +1,12 @@
+public class ejercicio1 {
+//Define un array de números tipo double de 3 filas por 7 columnas con nombre doub y asigna los valores según la siguiente tabla.
+// Muestra el contenido de todos los elementos del array dispuestos en forma de tabla como se muestra en la figura.
+
+    public static void main(String[] args){
+        double [] [] doub = {
+
+
+        }
+    }
+}
+
