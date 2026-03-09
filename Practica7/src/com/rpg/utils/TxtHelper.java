d7cfc43b1a89bcafdd7481f1316959fc57685624
RaquelPlata <raquelplataramos@gmail.com>
2026-03-09 11:39:46 +0100

    Terminado Txt.Helper

---
+++ Practica7/src/com/rpg/utils/TxtHelper.java
@@ -0,0 +1,47 @@
+package com.rpg.utils;
+import com.rpg.model.ciudades;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TxtHelper {
+
+    public TxtHelper(){ }
+
+    public static List<ciudades> leerciudades() {
+
+        List<ciudades> listaciudades = new ArrayList<>();
+
+        try {
+            List<String> lineas = Files.readAllLines(
+                    Paths.get("practica7/ficheros/cuidades.txt"));
+
+            for (String linea : lineas) {
+
+                String[] s = linea.split(";");
+
+                ciudades c = new ciudades(
+                        s[0],
+                        Integer.parseInt(s[1]),
+                        s[2],
+                        Integer.parseInt(s[3])
+                );
+
+                listaciudades.add(c);
+            }
+
+            for (ciudades c : listaciudades) {
+                System.out.println(c.getNombre());
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("No ha podido abrise el archivo: " + e.getMessage());
+            return listaciudades;
+        }
+
+        return listaciudades;
+    }
+}
