370fade708091fd782a71f916a8b41324d20d767
RaquelPlata <raquelplataramos@gmail.com>
2026-03-12 08:34:13 +0100

    GestionMundo echa falta revisarla

--- Practica7/src/com/rpg/services/GestionMundo.java
+++ Practica7/src/com/rpg/services/GestionMundo.java
@@ -1,4 +1,57 @@
 package com.rpg.services;
 
+import com.rpg.handler.DatoInvalidoException;
+import com.rpg.model.Item;
+import com.rpg.model.personaje;
+import com.rpg.model.ciudades;
+import com.rpg.utils.JsonHelper;
+import com.rpg.utils.TxtHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class GestionMundo {
+
+    private List<ciudades> listaCiudades;
+    private List<personaje> personajes;
+    private List<Item> listaItem;
+    private HashMap<String, Item> mapItems;
+
+    public GestionMundo() {
+        listaCiudades = new ArrayList<>();
+        personajes = new ArrayList<>();
+        listaItem = new ArrayList<>();
+        mapItems = new HashMap<>();
+    }
+
+    public void cargarTodo() {
+
+        listaCiudades = TxtHelper.leerciudades();
+        personajes = JsonHelper.leerPersonaje();
+        listaItem = JsonHelper.leerItem();
+
+        for (Item i : listaItem) {
+            mapItems.put(i.getId(), i);
+        }
+    }
+
+    public void crearPersonaje(String nombre, String raza, Integer nivel, List<String> idItems) throws DatoInvalidoException {
+
+        List<Item> equipo = new ArrayList<>();
+
+        for (String id : idItems) {
+
+            if (!mapItems.containsKey(id)) {
+                throw new DatoInvalidoException("El item no existe");
+            }
+
+            equipo.add(mapItems.get(id));
+        }
+
+        personaje p = new personaje(nombre, raza, nivel);
+        p.setEquipo(equipo);
+
+        personajes.add(p);
+    }
 }
