563132724df2532148ead5738fc1f3b91480ab7e
RaquelPlata <raquelplataramos@gmail.com>
2026-03-12 08:34:34 +0100

    main falta cambiar cosas

--- Practica7/src/Main.java
+++ Practica7/src/Main.java
@@ -1,15 +1,23 @@
-import com.rpg.utils.JsonHelper;
-import com.rpg.utils.TxtHelper;
+import com.rpg.handler.RPGDataException;
+import com.rpg.services.GestionMundo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Ciudades:");
-        TxtHelper t = new TxtHelper();
-        System.out.println(t.leerciudades());
-        System.out.println("Item:");
-        JsonHelper j = new JsonHelper();
-        System.out.println(j.leerItem());
-        System.out.println("personajes:");
-        System.out.println(j.leerPersonaje());
+
+    public static void main(String[] args) throws RPGDataException {
+
+        GestionMundo gestion = new GestionMundo();
+
+        gestion.cargarTodo();
+
+        gestion.crearPersonaje(
+                "Aragorn",
+                "Humano",
+                20,
+                new ArrayList<>(List.of( "E01"))
+        );
+
     }
 }
